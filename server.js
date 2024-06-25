da6e6b3a36f0c9a5a73f4508f826bd25ff9c145c
Deepak <djamwal10@gmail.com>
2024-06-25 22:17:32 +0530

    error handling

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000;
 /// middleware
 app.use(express.json()); // to parse req stream coming in json
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
     return console.log(`Express is running on port ${port}`);
