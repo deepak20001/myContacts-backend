96ff49f86f1f0568a170a809545bf3f4097161ff
Deepak <djamwal10@gmail.com>
2024-06-27 07:37:40 +0530

    user routes & controller created

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 /// middleware
 app.use(express.json()); // to parse req stream coming in json
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
